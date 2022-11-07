17ba2a6cda7b38f3a64210e875b7847df4181e76
nass <cabliveer@gmail.com>
2022-11-07 17:12:46 +0100

    ds

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -20,7 +20,7 @@ export default function About(props) {
 
 //const formattedCategories = categories.map((cat)=>cat.title).join('•')
 //const description = `${formattedCategories} ${price?'•'+price:""} • 🎫 • ${rating} ⭐ (${review_count}+)`
-const description = `⭐${rating} (${review_count}+ ratings) • ${categories[0].title} •${price}• 🎫`
+const description = `⭐${rating} (${review_count}+ ratings) • ${price}• 🎫`
   return (
      
     <View style={styles.container}>
